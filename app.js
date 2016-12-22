e79c0c26052facd756d482bff392197908c99b15
Vittorio <sexsportandrocknroll@gmail.com>
2016-12-22 21:43:16 +0300

    added members list compatibility

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var redisClient = redis.createClient();
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var port = 8082;
 
 function emitMessage(client, message) {
   client.emit('message', message);
@@ -14,20 +15,40 @@ function emitMessage(client, message) {
 io.on('connection', function (client) {
   console.log("Client connected");
   client.on('join', function (name) {
-    client.name = name || "Guest";
+    name = name || "Guest";
+    client.name = name;
     console.log(name + ' joined');
+
     redisClient.lrange('messages', 0, -1, function (err, data) {
       data.reverse().forEach(function (message) {
         emitMessage(client, message);
       });
     });
+
+    client.broadcast.emit('new member', name);
+    redisClient.smembers('members', function(err, members) {
+      members.forEach(function (member) {
+        client.emit('new member', member);
+      });
+    });
+
+    redisClient.sadd('members', name);
   });
 
   client.on('message', function (message) {
     var fullMessage = client.name + ": " + message;
+
     client.broadcast.emit('message', fullMessage);
     emitMessage(client, fullMessage);
     redisClient.lpush('messages', fullMessage)
+  });
+
+  client.on('disconnect', function (eventName) {
+    var name = client.name;
+
+    console.log('Member ' + name + " disconnected");
+    redisClient.srem('members', name);
+    client.broadcast.emit('remove member', name);
   });
 });
 
@@ -35,6 +56,6 @@ app.get('/', function(req, res) {
   res.render('index.ejs');
 });
 
-server.listen(8082, function () {
-  console.log('Listening on port 8082')
+server.listen(port, function () {
+  console.log('Listening on port ' + port);
 });
