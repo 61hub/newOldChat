8c9b0e6197bb90a55d41ae1af39fa856c439643a
Vittorio <sexsportandrocknroll@gmail.com>
2016-12-22 03:03:58 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+function emitMessage(client, message) {
+  client.broadcast.emit('message', message);
+  client.emit('message', message);
+}
+
+io.on('connection', function (client) {
+  console.log("Client connected");
+  client.on('join', function (name) {
+    client.name = name || "Guest";
+    redisClient.lrange('messages', 0, -1, function (err, data) {
+      data.reverse().forEach(function (message) {
+        emitMessage(client, message);
+      });
+    });
+  });
+
+  client.on('message', function (message) {
+    var fullMessage = client.name + ": " + message;
+    emitMessage(client, fullMessage);
+    redisClient.lpush('messages', fullMessage)
+  });
+});
+
+app.get('/', function(req, res) {
+  res.render('index.ejs');
+});
+
+server.listen(8082, function () {
+  console.log('Listening on port 8082')
+});
