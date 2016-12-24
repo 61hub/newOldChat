5b37c379e468d70333c90a4f02c6b082f094834e
Kate <katerina_ti@mail.ru>
2016-12-24 19:28:05 +0300

    #5 was fixed

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,25 @@
 var socketServer = io.connect();
 console.log(socketServer);
+function sendMessage () {
+    var input = document.getElementsByTagName("input")[0];
+    if (input.value.length > 0) {
+        socketServer.emit("message", input.value);
+        input.value = "";
+    }
+}
 
 var userName = prompt("What`s your name?");
 socketServer.emit("join", userName);
 socketServer.on("message", function (message) {
     var messages = document.getElementsByClassName("messages")[0];
     messages.innerHTML += "<div>" + message + "</div>";
-})
+});
 var button = document.getElementsByTagName("button")[0];
-button.addEventListener("click", function () {
-    var input = document.getElementsByTagName("input")[0];
-    if (input.value.length > 0) {
-        socketServer.emit("message", input.value);
-        input.value = "";
-    }
+button.addEventListener("click", sendMessage () );
 
-})
+var form = document.getElementById("send");
+form.addEventListener("submit", sendMessage(event){
+    event.preventDefault();
+    }
+)
 
